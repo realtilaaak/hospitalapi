cbe783dac79e7a548fdf232638584e912bdc494f
realtilaak <thakur.tilak3551@gmail.com>
2026-05-26 16:27:54 +0530

    billing backend added

--- src/main/java/com/hospital/hospitalapi/model/Invoice.java
+++ src/main/java/com/hospital/hospitalapi/model/Invoice.java
@@ -1,37 +1,83 @@
-package com.hospital.hospitalapi.controller;
+package com.hospital.hospitalapi.model;
 
-import com.hospital.hospitalapi.model.Medicine;
-import com.hospital.hospitalapi.repository.MedicineRepository;
+import jakarta.persistence.*;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+@Entity
+public class Invoice {
 
-import java.util.List;
+    @Id
+    @GeneratedValue(
+            strategy =
+                    GenerationType.IDENTITY
+    )
+    private Long id;
 
-@RestController
-@RequestMapping("/medicines")
-@CrossOrigin(origins = "*")
-public class MedicineController {
+    private String patient;
+    private String doctor;
+    private String amount;
+    private String date;
+    private String payment;
 
-    @Autowired
-    private MedicineRepository repo;
-
-    @GetMapping
-    public List<Medicine> getAllMedicines() {
-        return repo.findAll();
+    public Invoice() {
     }
 
-    @PostMapping
-    public Medicine addMedicine(
-            @RequestBody Medicine medicine
-    ) {
-        return repo.save(medicine);
+    public Long getId() {
+        return id;
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteMedicine(
-            @PathVariable Long id
+    public void setId(
+            Long id
     ) {
-        repo.deleteById(id);
+        this.id = id;
+    }
+
+    public String getPatient() {
+        return patient;
+    }
+
+    public void setPatient(
+            String patient
+    ) {
+        this.patient = patient;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(
+            String doctor
+    ) {
+        this.doctor = doctor;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(
+            String amount
+    ) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(
+            String date
+    ) {
+        this.date = date;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(
+            String payment
+    ) {
+        this.payment = payment;
     }
 }
